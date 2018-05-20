0d4e3645dfe9690630861fce3234f5b7ba7b93f6
Ben Chatelain <ben@octop.ad>
2018-05-20 10:14:12 -0600

    🐛 Add return to only pick up first loction

--- .fork/settings.gradle.kts
+++ .fork/settings.gradle.kts
@@ -6,15 +6,19 @@
 rootProject.name = "xcodePluginFork"
 
 // Include fork project in composite build from any of these locations
-listOf(
-    file("../../Fork"),
-    file("build/dependencies/Fork")
-).forEach {
-    if (it.exists()) {
-        includeBuild(it) {
-            dependencySubstitution {
-                substitute(module("at.phatbl:fork")).with(project(":"))
+run loop@{
+    listOf(
+        file("../../Fork"),
+        file("build/dependencies/Fork")
+    ).forEach {
+        if (it.exists()) {
+            includeBuild(it) {
+                dependencySubstitution {
+                    substitute(module("at.phatbl:fork")).with(project(":"))
+                }
             }
+            // Break out of forEach to only take the first location found
+            return@loop
         }
     }
 }
